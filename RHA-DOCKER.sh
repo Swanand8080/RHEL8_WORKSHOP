86a0868d2bc69780b298aafbd1e7026e4e8e7439
Unnati-TD <103584661+Unnati-TD@users.noreply.github.com>
2022-04-19 21:00:36 +0530

    Update RHA-DOCKER.sh

--- RHA-DOCKER.sh
+++ RHA-DOCKER.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-if
 echo "Enter your name"
 read name
 echo "Enter your email"
 read email
 echo "Enter you college name"
 read college
-fi
+mkdir /rhel8_data
+apk add nfs-utils 
+mount 3.224.211.85:/nfsdata /rhel8_data
+mkdir /rhel8_data/$email
+ls /rhel8_data
+
 #Task 1
 #checking for /var/tmp/passwd
 check=0
@@ -32,6 +36,7 @@ fi
 if [ "$check" == "3" ]
 then
         echo -e "\e[1;32m-------Task2 completed successfully-------\e[0m"
+	touch /rhel8_data/$email/1.txt
 else
         echo -e "\e[1;31m-------Task2 did not completed successfully-------\e[0m"
 fi
@@ -66,6 +71,7 @@ done < $FILENAME
 if [ "$check" == "3" ]
 then
         echo -e "\e[1;32m-------Task4 completed successfully-------\e[0m"
+	touch /rhel8_data/$email/2.txt
 else
         echo -e "\e[1;31m-------Task4 did not completed successfully-------\e[0m"
 fi
@@ -76,6 +82,7 @@ v5=$(hostname | grep rhel8.example.com)
 if [ $? == 0 ]
 then
         echo -e "\e[1;32m-------Task6 completed successfully-------\e[0m"
+	touch /rhel8_data/$email/3.txt
 else
         echo -e "\e[1;31m-------Task6 did not completed successfully-------\e[0m"
 fi
@@ -86,6 +93,7 @@ v6=$(grep "WELCOME TO RHEL8 WORKSHOP" /etc/motd)
 if [ $? == 0 ]
 then
 echo -e "\e[1;32m-------Task7 completed successfully-------\e[0m"
+touch /rhel8_data/$email/4.txt
 else
 echo -e "\e[1;31m-------Task7 did not completed successfully-------\e[0m"
 fi
